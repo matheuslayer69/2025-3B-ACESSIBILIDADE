493d8d928588206201523a2df3914b583c499a6a
matheuslayer69 <joao.dasilva@escola.pr.gov.br>
2025-03-14 09:30:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const aumentaFonteBotao = document.getElementById('aumentar-fonte')
+
+let tamanhoFonteAtual = 1; 
+
+aumentaFonteBotao.addEventListener ('click',function() {
+    tamanhoFonteAtual +=0.1;
+    document.body.style.fontize= `${tamanhoFonteAtual}rem`
+
+})
